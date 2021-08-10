271c6f7530488e79de34322812d107f895fc53ea
parasuram <rampikkili57@gmail.com>
2021-08-10 21:49:38 +0530

    first

---
+++ src/com/Exe2.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exe2 {
+
+	//public
+	final int j=1;
+	
+	public Exe2(int j) {
+		
+		 final int i=this.j;
+		 System.out.println(""+i);
+	}
+	//i=1;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exe2 e=new Exe2(10);
+
+		System.out.println(e instanceof Exe2);
+		
+		List<Integer> l=new ArrayList<>();
+		l.add(10);
+		l.add(20);
+		
+		l.stream().St
+		
+	}
+
+}
